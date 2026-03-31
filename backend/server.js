a14c52781d21a270ad1a6f7b28da3412c215d188
gabadedibu <gabadedibu@gmail.com>
2026-04-01 00:34:26 +0100

    fix: remove request logging middleware for cleaner code

--- backend/server.js
+++ backend/server.js
@@ -82,10 +82,7 @@ app.use(
   '/uploads/profile-pictures',
   express.static(path.join(__dirname, 'uploads/profile-pictures'))
 );
-app.use((req, res, next) => {
-  console.log("REQUEST:", req.method, req.url);
-  next();
-});
+
 
 // Serve media files from 'message-uploads' folder
 app.use('/uploads/message-uploads', express.static(path.join(__dirname, 'uploads')));
