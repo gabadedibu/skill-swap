3863c9517e4abf1de340f50a8373674f6f9ec20a
gabadedibu <gabadedibu@gmail.com>
2026-04-01 00:27:42 +0100

    fix: add middleware to log incoming requests for better debugging

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,10 @@ app.use(
   '/uploads/profile-pictures',
   express.static(path.join(__dirname, 'uploads/profile-pictures'))
 );
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
 
 // Serve media files from 'message-uploads' folder
 app.use('/uploads/message-uploads', express.static(path.join(__dirname, 'uploads')));
